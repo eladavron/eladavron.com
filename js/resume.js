a8f6ad6d8960695ee588634ac2e5e2d36ddaf277
Elad Avron <eladavron@gmail.com>
2020-02-08 17:14:26 +0200

    Initial font work

--- js/resume.js
+++ js/resume.js
@@ -1,17 +1,3 @@
-
-$(document).ready(function () {
-  $('.social-icons').children('a').hover(
-    function () {
-      $(this).children("div").animate({
-        opacity: '1'
-      })
-    },
-    function () {
-      $(this).children("div").animate({
-        opacity: '0'
-      });
-    }
-  );
-
-  $('.jtextfill').textfill({ maxFontPixels: 36 });
-});
+$(document).ready(function(){
+    $("[data-toggle='tooltip']").tooltip()
+})
